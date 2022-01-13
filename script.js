2f189a1f08b33ed14b86094b124a4896ff256bf3
Gina.Chen/陳靓 <Gina.Chen@JMC.LOCAL>
2022-01-13 15:21:40 +0800

    update function for operators, decimal and display

--- script.js
+++ script.js
@@ -2,17 +2,62 @@ const calculatorDisplay = document.querySelector('h1');
 const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clear-btn');
 
+let firstValue = 0;
+let operatorValue = '';
+let awaitingNextValue = false;
+
 function sendNumberValue(number) {
-	// if currentDisply value is 0, replace it, if not add number
-	const displayValue = calculatorDisplay.textContent;
-	calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+	//replace current display value if first value is entered
+	if(awaitingNextValue) {
+		calculatorDisplay.textContent = number;
+		awaitingNextValue = false;
+	}else{
+		// if currentDisply value is 0, replace it, if not add number
+		const displayValue = calculatorDisplay.textContent;
+		calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+	}
 }
 
 function addDecimal() {
+	//If operator pressed, don't add decimal
+	if(awaitingNextValue) return;
 	//If no decimal, add one
 	if(!calculatorDisplay.textContent.includes('.')) {
 		calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
 	}
+}
+
+// calculate first and second values depending on operator
+const calculate = {
+	'/': (firstNumber, secondNumber) => firstNumber / secondNumber,
+	
+	'*': (firstNumber, secondNumber) => firstNumber * secondNumber,
+	
+	'+': (firstNumber, secondNumber) => firstNumber + secondNumber,
+	
+	'-': (firstNumber, secondNumber) => firstNumber - secondNumber,
+	
+	'=': (firstNumber, secondNumber) => secondNumber,
+}
+
+function useOperator(operator) {
+	const currentValue = Number(calculatorDisplay.textContent);
+	//prevent multiple operators
+	if(operatorValue && awaitingNextValue) {
+		operatorValue = operator;
+		return;
+	}
+	// Assign first value if no value
+	if(!firstValue) {
+		firstValue = currentValue;
+	}else {
+		const calculation = calculate[operatorValue](firstValue, currentValue);
+		calculatorDisplay.textContent = calculation
+		firstValue = calculation;
+	}
+	//ready for next value, store operator
+	awaitingNextValue = true;
+	operatorValue = operator;
 }
 
 // add Event Listeners for numbers,operators, decimal buttons
@@ -20,14 +65,17 @@ inputBtns.forEach((inputBtn) => {
   if(inputBtn.classList.length === 0) {
   	inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
   }else if (inputBtn.classList.contains('operator')) {
-  	inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+  	inputBtn.addEventListener('click', () => useOperator(inputBtn.value));
   }else if (inputBtn.classList.contains('decimal')) {
   	inputBtn.addEventListener('click', () => addDecimal());
   }
 });
 
-// Reset Display
+// Reset all values, display
 function resetAll() {
+	firstValue = 0;
+	operatorValue = '';
+	awaitingNextValue = false;
 	calculatorDisplay.textContent = '0';
 }
 
